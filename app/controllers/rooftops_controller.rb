14753f0932133cbb2efad00e04959ea13262fd69
Grace Mia <grace.mehrabe@gmail.com>
2019-02-18 16:30:29 +0100

    added new and create in rooftops controller

--- app/controllers/rooftops_controller.rb
+++ app/controllers/rooftops_controller.rb
@@ -1,3 +1,16 @@
 class RooftopsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
+  def new
+    @rooftop = Rooftop.new
+  end
+
+  def create
+    @rooftop = Rooftop.new(rooftop_params)
+    if @rooftop.save
+      redirect_to rooftop_path(@rooftop)
+    else
+      render :new
+    end
+  end
 end
