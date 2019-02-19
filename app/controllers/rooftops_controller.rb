57623c15281a0b308f50c9e25866b885d64ebe27
Grace Mia <grace.mehrabe@gmail.com>
2019-02-19 10:44:51 +0100

    Added pundit plus controllers routes

--- app/controllers/rooftops_controller.rb
+++ app/controllers/rooftops_controller.rb
@@ -1,12 +1,12 @@
 class RooftopsController < ApplicationController
-  
+
   skip_before_action :authenticate_user!, only: [:show, :index]
   before_action :set_rooftop, only: [:show, :edit, :update, :destroy]
-  
+
   def index
-    @rooftops = Rooftop.all
+    @rooftops = policy_scope(Rooftop).order(created_at: :desc)
   end
-  
+
   def show
     @rooftop = Rooftop.find(params[:id])
   end
@@ -25,14 +25,27 @@ class RooftopsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @rooftop.update_attributes(rooftop_params)
+    redirect_to rooftop_path(@rooftop)
+  end
+
+  def destroy
+    @rooftop.destroy
+    redirect_to rooftops_path
+  end
+
   private
 
   def set_rooftop
     @rooftop = Rooftop.find(params[:id])
+    authorize @rooftop
   end
 
   def rooftop_params
     params.require(:rooftop).permit(:name, :address, :description, :price_per_hour, :photo)
   end
-
 end
