9d297b1c718da1a123196e7491d23e64993bdfab
Elie Alloun <eliealloun21@gmail.com>
2019-02-19 15:58:41 +0100

    sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,17 @@
 import "bootstrap";
 
 import { loadDynamicBannerText } from '../components/banner';
+import { initSweetalert } from '../components/init_sweetalert';
+
 loadDynamicBannerText();
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
