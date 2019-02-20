2831ef11db1188069dd90b67dcd9fdd344f4bf9c
Grace Mia <grace.mehrabe@gmail.com>
2019-02-20 11:36:52 +0100

    reservations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:myrooftops]
+  skip_before_action :authenticate_user!, only: [:myrooftops, :myreservations]
 
   def myrooftops
     skip_authorization
     @user = current_user
     @rooftops = @user.rooftops
   end
+
+  def myreservations
+    skip_authorization
+    @user = current_user
+    @rooftops = @user.rooftops
+  end
+
 end
